1cde650827c98dd17108e40463cfd5abcf6b0f5e
Alejandro Sanchez <pmisson@gmail.com>
2015-06-06 21:26:54 -0400

    Add 3d plane fits

--- kolorespy.py
+++ kolorespy.py
@@ -170,24 +170,104 @@ DMSP_1999=asciitable.read("DMSP_1999.csv")
 #
 ####################
 
+##########################
+#
+#
+# Fiting planes from: http://stackoverflow.com/questions/15959411/fit-points-to-a-plane-algorithms-how-to-iterpret-results
+#
+###############################33
+
+
+import numpy as np
+import scipy.optimize
+
+def fitPLaneLTSQ(XYZ):
+    # Fits a plane to a point cloud, 
+    # Where Z = aX + bY + c        ----Eqn #1
+    # Rearanging Eqn1: aX + bY -Z +c =0
+    # Gives normal (a,b,-1)
+    # Normal = (a,b,-1)
+    [rows,cols] = XYZ.shape
+    G = np.ones((rows,3))
+    G[:,0] = XYZ[:,0]  #X
+    G[:,1] = XYZ[:,1]  #Y
+    Z = XYZ[:,2]
+    (a,b,c),resid,rank,s = np.linalg.lstsq(G,Z) 
+    normal = (a,b,-1)
+    nn = np.linalg.norm(normal)
+    normal = normal / nn
+    return normal
+
+
+def fitPlaneSVD(XYZ):
+    [rows,cols] = XYZ.shape
+    # Set up constraint equations of the form  AB = 0,
+    # where B is a column vector of the plane coefficients
+    # in the form b(1)*X + b(2)*Y +b(3)*Z + b(4) = 0.
+    p = (np.ones((rows,1)))
+    AB = np.hstack([XYZ,p])
+    [u, d, v] = np.linalg.svd(AB,0)        
+    B = v[3,:];                    # Solution is last column of v.
+    nn = np.linalg.norm(B[0:3])
+    B = B / nn
+    return B[0:3]
+
+
+def fitPlaneEigen(XYZ):
+    # Works, in this case but don't understand!
+    average=sum(XYZ)/XYZ.shape[0]
+    covariant=np.cov(XYZ - average)
+    eigenvalues,eigenvectors = np.linalg.eig(covariant)
+    want_max = eigenvectors[:,eigenvalues.argmax()]
+    (c,a,b) = want_max[3:6]    # Do not understand! Why 3:6? Why (c,a,b)?
+    normal = np.array([a,b,c])
+    nn = np.linalg.norm(normal)
+    return normal / nn  
+
+def fitPlaneSolve(XYZ):
+    X = XYZ[:,0]
+    Y = XYZ[:,1]
+    Z = XYZ[:,2] 
+    npts = len(X)
+    A = np.array([ [sum(X*X), sum(X*Y), sum(X)],
+                   [sum(X*Y), sum(Y*Y), sum(Y)],
+                   [sum(X),   sum(Y), npts] ])
+    B = np.array([ [sum(X*Z), sum(Y*Z), sum(Z)] ])
+    normal = np.linalg.solve(A,B.T)
+    nn = np.linalg.norm(normal)
+    normal = normal / nn
+    return normal.ravel()
+
+def fitPlaneOptimize(XYZ):
+    def residiuals(parameter,f,x,y):
+        return [(f[i] - model(parameter,x[i],y[i])) for i in range(len(f))]
+
+
+    def model(parameter, x, y):
+        a, b, c = parameter
+        return a*x + b*y + c
+
+    X = XYZ[:,0]
+    Y = XYZ[:,1]
+    Z = XYZ[:,2]
+    p0 = [1., 1.,1.] # initial guess
+    result = scipy.optimize.leastsq(residiuals, p0, args=(Z,X,Y))[0]
+    normal = result[0:3]
+    nn = np.linalg.norm(normal)
+    normal = normal / nn
+    return normal
+
+
+#####################################3
+#
+# Fiting planes End
+#
+#######################333
 def normaliza(X):
     max=np.max(X.intrel)
     X.intrel=X.intrel/max
     return X
-if 0:
-    fig, (ax0,ax1,ax2) = plt.subplots(nrows=3)
 
-    ax0.plot(WL_VEGA_AB, FLUX_VEGA_AB)
-    ax0.plot(WL_VEGA_ST, FLUX_VEGA_ST)
-    ax0.set_title('Reference AB')
-    ax1.plot(spectro.col2, spectro.col1)
-    ax1.set_title('Spectra')
-    ax2.plot(photo.wave,photo.intrel)
-    ax2.plot(psas.wave,psas.intrel)
-    ax2.plot(scoto.wave,scoto.intrel)
-    ax2.plot(msas.wave,msas.intrel)
-    ax2.set_title('Filter')
-    plt.show()
 #AB_ref = np.array(WL_VEGA_AB, dtype=[('wave', float)])
 def mag(filtroX,espectro,referencia): #Calculate the sintetic magnitude and also the Intensity.
     if 0:
@@ -213,7 +293,7 @@ def mag(filtroX,espectro,referencia): #Calculate the sintetic magnitude and also
     Up = quad(S1_1, xs[0], xs[-1])
     Dn = quad(S1_2, xs[0], xs[-1])
     m=-2.5*np.log10(Up[0]/Dn[0])
-    I=Up[0]/Dn[0]
+    I=10**(m/-2.5)
     return m,I
 
 def IndiceIsoLux(filtroX,espectro,referencia,photo): #Calculate Index as is definide in http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0067798
@@ -268,26 +348,44 @@ def calculamagnitudes(filename):
     i_sli=IndiceIsoLux(scoto,spectro,sun_lambda,photo)
     i_msi=IndiceIsoLux(msas,spectro,sun_lambda,photo)
     i_ipi=IndiceIsoLux(psas,spectro,sun_lambda,photo)
-    return Uj_Vj,Bj_Vj,Bn_Vj,Rn_Vj,SQM_Vj,Bn_Gn,Gn_Rn,i_sli,i_msi,i_ipi
+    if 0:
+        fig, (ax0,ax1,ax2) = plt.subplots(nrows=3)
+
+        ax0.plot(WL_VEGA_AB, FLUX_VEGA_AB)
+        ax0.plot(WL_VEGA_ST, FLUX_VEGA_ST)
+        ax0.set_title(filename)
+        ax1.plot(spectro.col2, spectro.col1)
+        ax1.set_title('Spectra')
+        ax2.plot(photo.wave,photo.intrel)
+        ax2.plot(psas.wave,psas.intrel)
+        ax2.plot(scoto.wave,scoto.intrel)
+        ax2.plot(msas.wave,msas.intrel)
+        ax2.set_title('Filter')
+        plt.show()
+    return Uj_Vj,Bj_Vj,Bj_Vj,Rj_Vj,SQM_Vj,m_Bn,m_Gn,m_Rn,i_sli,i_msi,i_ipi,Bn_Vj,Gn_Vj,Rn_Vj
 
 path=os.getcwd()
 lista=os.listdir(path)
 lista2=" ".join(lista)
-lista3=re.findall(r'[a-zA-Z_0-9]+.dat',lista2)
+lista3=re.findall(r'[a-zA-Z_0-9]+\.dat',lista2)
 lista3.sort()
 listafiles=lista3
 
 
 
-spec=np.recarray((len(listafiles),),dtype=[(name, S20),('Uj_Vj', float), ('Bj_Vj', float),('Rn_Vj', float), ('SQM_Vj', float), ('Bn_Gn', float), ('Gn_Rn', float),('i_sli', float),('i_msi', float),('i_ipi', float)])
+spec=np.recarray((len(listafiles),),dtype=[('num', int),('name', 'S100'),('Uj_Vj', float), ('Bj_Vj', float),('Rj_Vj', float), ('SQM_Vj', float), ('m_Bn', float), ('m_Gn', float),('m_Rn', float),('i_sli', float),('i_msi', float),('i_ipi', float),('Bn_Vj', float),('Gn_Vj', float),('Rn_Vj', float)])
 for X in range(len(listafiles)):
+    spec[X].num=X
     spec[X].name=listafiles[X].split('.')[0]
-    spec[X].Uj_Vj,spec[X].Bj_Vj,spec[X].Rn_Vj,spec[X].SQM_Vj,spec[x].Bn_Gn,spec[X].Gn_Rn,spec[X].i_sli,spec[X].i_msi,spec[X].i_ipi=calculamagnitudes(listafiles[X])
+    spec[X].Uj_Vj,spec[X].Bj_Vj,spec[X].Bj_Vj,spec[X].Rn_Vj,spec[X].SQM_Vj,spec[X].m_Bn,spec[X].m_Gn,spec[X].m_Rn,spec[X].i_sli,spec[X].i_msi,spec[X].i_ipi,spec[X].Bn_Vj,spec[X].Gn_Vj,spec[X].Rn_Vj=calculamagnitudes(listafiles[X])
+
+spec = spec[np.array(np.ones(len(spec))-np.isnan(spec['m_Bn'])).astype(bool)]
 
 # Canales DMSP/OLS y VIIRS
 # Espectros de lamparas
 # Respuesta de Nikon D3s de LICA
 # Colores esperados en ISS pictures
+#spec= spec[1-np.isnan(spec['Uj_Vj'])]
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, show, rc, grid, savefig
@@ -313,18 +411,278 @@ plt.rcParams['xtick.major.size']=8
 plt.rcParams['figure.figsize'] = 9,9
 plt.rcParams['figure.subplot.bottom'] = 0.15
 #plt.rcParams['ytick.labelsize'] = 10
+BG=((10**((spec.m_Bn/-2.5)))/10**((spec.m_Gn/-2.5)))
+GR=((10**((spec.m_Gn/-2.5)))/10**((spec.m_Rn/-2.5)))
 
-
-plt.plot(spec.Bn_Gn,spec.Gn_Rn,'ko')
+plt.plot(BG,GR,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(BG[X],GR[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
 plt.xlabel("B/G")
 plt.ylabel("G/R")
 plt.title("ISS colors for typical lamps")
-plt.xlim(0,0.7)
-plt.ylim(0,1.4)
+plt.xlim(0,0.9)
+plt.ylim(0,2.1)
 plt.savefig('colores_espectros.png')
 plt.show()    
     
 
+plt.plot(BG,spec.i_sli,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(BG[X],spec.i_sli[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("B/G")
+plt.ylabel("SLI")
+plt.title("SLI")
+plt.xlim(0,0.9)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_SLI_BG.png')
+plt.show()
+
+plt.plot(GR,spec.i_sli,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(GR[X],spec.i_sli[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("G/R")
+plt.ylabel("SLI")
+plt.title("SLI")
+plt.xlim(0,2.1)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_SLI_GR.png')
+plt.show()
+###################################################3
+
+    
+
+plt.plot(BG,spec.i_ipi,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(BG[X],spec.i_ipi[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("B/G")
+plt.ylabel("IPI")
+plt.title("IPI")
+plt.xlim(0,0.9)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_IPI_BG.png')
+plt.show()
+
+plt.plot(GR,spec.i_ipi,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(GR[X],spec.i_ipi[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("G/R")
+plt.ylabel("IPI")
+plt.title("IPI")
+plt.xlim(0,2.1)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_IPI_GR.png')
+plt.show() 
+###################################################3
+
+    
+
+plt.plot(BG,spec.i_msi,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(BG[X],spec.i_msi[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("B/G")
+plt.ylabel("MSI")
+plt.title("MSI")
+plt.xlim(0,0.9)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_MSI_BG.png')
+plt.show()
+
+plt.plot(GR,spec.i_sli,'ko')
+if 1:
+ for X in range(len(listafiles)):
+   try:
+    plt.text(GR[X],spec.i_msi[X]+0.01,str(spec[X].num),ha='right', va='bottom')
+   except:
+    print "fallo"
+plt.xlabel("G/R")
+plt.ylabel("MSI")
+plt.title("MSI")
+plt.xlim(0,2.1)
+plt.ylim(0,1.2)
+plt.savefig('colores_espectros_MSI_GR.png')
+plt.show()   
+
+asciitable.write(spec,'espectros.txt')
 
 
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+#X, Y, Z = axes3d.get_test_data(0.05)
+ax.scatter(BG, GR, spec.i_msi, c='r', marker='o')
+
+ax.set_xlabel('B/G')
+ax.set_ylabel('G/R')
+ax.set_zlabel('MSI')
+ax.set_xlim(0,1)
+ax.set_ylim(0,2.1)
+ax.set_zlim(0,1.2)
+
+plt.show()
+
+XYZ=np.vstack((BG,GR,spec.i_msi))
+
+
+XYZ1=XYZ.transpose()
+XYZ2=[]
+
+
+plane=fitPlaneOptimize(XYZ1)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+point  = np.mean(XYZ1,0)
+normal = fitPLaneLTSQ(XYZ1)
+
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = -point.dot(normal)
+
+# create x,y
+xx, yy = np.meshgrid(range(3), range(3))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+plt3d = plt.figure().gca(projection='3d')
+
+#plt3d.plot_surface(xx, yy, z)
+plt3d.plot_wireframe(xx, yy, z, rstride=1, cstride=1)
+plt3d.scatter(BG, GR, spec.i_msi, c='r', marker='o')
+plt3d.set_title('MSI fit by B/G and G/R')
+plt3d.set_xlim(0,1)
+plt3d.set_ylim(0,2.1)
+plt3d.set_zlim(0,1.2)
+plt3d.set_xlabel('B/G')
+plt3d.set_ylabel('G/R')
+plt3d.set_zlabel('MSI')
+plt.show()
+
+
+##################################
+#
+# Fiting plane IPI
+#
+####################################
+
+XYZ=np.vstack((BG,GR,spec.i_ipi))
+
+
+XYZ1=XYZ.transpose()
+XYZ2=[]
+
+
+plane=fitPlaneOptimize(XYZ1)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+point  = np.mean(XYZ1,0)
+normal = fitPLaneLTSQ(XYZ1)
+
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = -point.dot(normal)
+
+# create x,y
+xx, yy = np.meshgrid(range(3), range(3))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+plt3d = plt.figure().gca(projection='3d')
+
+#plt3d.plot_surface(xx, yy, z)
+plt3d.plot_wireframe(xx, yy, z, rstride=1, cstride=1)
+plt3d.scatter(BG, GR, spec.i_msi, c='r', marker='o')
+plt3d.set_title('IPI fit by B/G and G/R')
+plt3d.set_xlim(0,1)
+plt3d.set_ylim(0,2.1)
+plt3d.set_zlim(0,1.2)
+plt3d.set_xlabel('B/G')
+plt3d.set_ylabel('G/R')
+plt3d.set_zlabel('IPI')
+plt.show()
+
+
+##################################
+#
+# Fiting plane SLI
+#
+####################################
+
+XYZ=np.vstack((BG,GR,spec.i_sli))
+
+
+XYZ1=XYZ.transpose()
+XYZ2=[]
+
+
+plane=fitPlaneOptimize(XYZ1)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+point  = np.mean(XYZ1,0)
+normal = fitPLaneLTSQ(XYZ1)
+
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = -point.dot(normal)
+
+# create x,y
+xx, yy = np.meshgrid(range(3), range(3))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+plt3d = plt.figure().gca(projection='3d')
+
+#plt3d.plot_surface(xx, yy, z)
+plt3d.plot_wireframe(xx, yy, z, rstride=1, cstride=1)
+plt3d.scatter(BG, GR, spec.i_sli, c='r', marker='o')
+plt3d.set_title('SLI fit by B/G and G/R')
+plt3d.set_xlim(0,1)
+plt3d.set_ylim(0,2.1)
+plt3d.set_zlim(0,1.2)
+plt3d.set_xlabel('B/G')
+plt3d.set_ylabel('G/R')
+plt3d.set_zlabel('SLI')
+plt.show()
